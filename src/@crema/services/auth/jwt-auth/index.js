45a44d37bdf0ba9150f8c23a197a5e9e06ff92ea
Prajakta_Laptop <prajaktaweb@gmail.com>
2023-07-23 10:02:45 +0530

    BAck First Up 2

--- src/@crema/services/auth/jwt-auth/index.js
+++ src/@crema/services/auth/jwt-auth/index.js
@@ -6,7 +6,7 @@ const jwtAxios = axios.create({
   baseURL:
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:4000/api/'
-      : 'https://agilewater.vercel.app/api', // YOUR_API_URL HERE
+      : 'https://agilewaterback.vercel.app/api', // YOUR_API_URL HERE
   headers: {
     'Content-Type': 'application/json',
   },

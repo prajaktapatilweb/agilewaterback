d719283b720986ae1b7716d0968da3a1296c463c
Prajakta_Laptop <prajaktaweb@gmail.com>
2023-07-27 21:09:56 +0530

    get route code changed 1

--- server/routes/api/courses.js
+++ server/routes/api/courses.js
@@ -92,8 +92,71 @@ router.get("/getcourslist", auth, async (req, res) => {
   console.log("In request Get Course List ");
   res.setHeader("Access-Control-Allow-Origin", "*");
   try {
-    updateStatus();
-    getCoursList(req, res);
+    // updateStatus();
+    // getCoursList(req, res);
+    let NewList = await CoursesList.aggregate([
+      { $match: { Status: "Active" } },
+      {
+        $project: {
+          _id: 0,
+          CourseID: 1,
+          SdateParts: { $dateToParts: { date: "$StartDate" } },
+          EdateParts: { $dateToParts: { date: "$EndDate" } },
+          CourseName: 1,
+          EventDeleteDate: 1,
+          Place: 1,
+          Cost: 1,
+          Date: 1,
+          Time: 1,
+          Trainer: 1,
+        },
+      },
+      {
+        $addFields: {
+          Date: {
+            $function: {
+              body: function (StartDate, EndDate) {
+                let mlst = [, "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Sep", "Nov", "Dec"];
+                let Eyear = `${EndDate.year}`.slice(2, 4);
+                let Syear = `${StartDate.year}`.slice(2, 4);
+                let datefinal =
+                  StartDate.year === EndDate.year
+                    ? StartDate.month === EndDate.month
+                      ? `${StartDate.day} - ${EndDate.day} ${mlst[EndDate.month]} ${Eyear}`
+                      : `${StartDate.day} ${mlst[StartDate.month]} - ${EndDate.day} ${mlst[EndDate.month]} ${Eyear}`
+                    : `${StartDate.day} ${mlst[StartDate.month]} ${Syear}- ${EndDate.day} ${mlst[EndDate.month]} ${Eyear}`;
+
+                return datefinal;
+              },
+              args: ["$SdateParts", "$EdateParts"],
+              lang: "js",
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          CourseID: 1,
+          CourseName: 1,
+          Place: 1,
+          Cost: 1,
+          Date: 1,
+          Time: 1,
+          Trainer: 1,
+          // isExpired: {
+          //   // new field
+          //   $cond: {
+          //     if: { $gt: ["$EventDeleteDate", new Date()] },
+          //     then: false,
+          //     else: true,
+          //   },
+          // },
+        },
+      },
+    ]);
+    console.log("first", NewList.length);
+
+    return res.status(200).json({ List: NewList });
   } catch (err) {
     // logger.error(`Catch Block - User List Request Block ${err}`, { by: req.user.gid, for: [0], info: {} })
     return res.status(500).json({ error: `Server Error: ${err}` });
@@ -143,7 +206,7 @@ router.post("/addnewcourse", auth, async (req, res) => {
     data.Created.ByName = req.user.name;
     FinalData = new CoursesList(data);
     console.log("Final Data", FinalData);
-    let CourseData = await CoursesList.findOne({ CourseID: 'ID-2' });
+    let CourseData = await CoursesList.findOne({ CourseID: "ID-2" });
     await FinalData.save()
       .then(() => {
         return res.status(200).json({ data: "Success", CourseData: CourseData });

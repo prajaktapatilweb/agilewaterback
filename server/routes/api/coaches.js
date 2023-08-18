30ace7e5c74d09fe1d5590471afe975fd4f951ec
Prajakta_Laptop <prajaktaweb@gmail.com>
2023-08-18 23:37:31 +0530

    Coach List Add Edit Del Copleted BB

--- server/routes/api/coaches.js
+++ server/routes/api/coaches.js
@@ -7,80 +7,17 @@ const moment = require("moment");
 const CoachesList = require("../../models/CoachesList");
 const uploadController = require("../../middleware/uploadMultipleFiles");
 
-
-async function getCoursList(req, res) {
-  console.log("In get Course List");
-  let NewList = await CoursesList.aggregate([
-    { $match: { Status: "Active" } },
-    {
-      $project: {
-        _id: 0,
-        CourseID: 1,
-        SdateParts: { $dateToParts: { date: "$StartDate" } },
-        EdateParts: { $dateToParts: { date: "$EndDate" } },
-        CourseName: 1,
-        EventDeleteDate: 1,
-        Place: 1,
-        ActualCost: 1,
-        DiscountedCost: 1,
-        Date: 1,
-        Time: 1,
-        Trainer: 1,
-      },
-    },
-    {
-      $addFields: {
-        Date: {
-          $function: {
-            body: function (StartDate, EndDate) {
-              let mlst = [, "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Sep", "Nov", "Dec"];
-              let Eyear = `${EndDate.year}`.slice(2, 4);
-              let Syear = `${StartDate.year}`.slice(2, 4);
-              let datefinal =
-                StartDate.year === EndDate.year
-                  ? StartDate.month === EndDate.month
-                    ? `${StartDate.day} - ${EndDate.day} ${mlst[EndDate.month]} ${Eyear}`
-                    : `${StartDate.day} ${mlst[StartDate.month]} - ${EndDate.day} ${mlst[EndDate.month]} ${Eyear}`
-                  : `${StartDate.day} ${mlst[StartDate.month]} ${Syear}- ${EndDate.day} ${mlst[EndDate.month]} ${Eyear}`;
-
-              return datefinal;
-            },
-            args: ["$SdateParts", "$EdateParts"],
-            lang: "js",
-          },
-        },
-      },
-    },
-    {
-      $project: {
-        CourseID: 1,
-        CourseName: 1,
-        Place: 1,
-        ActualCost: 1,
-        DiscountedCost: 1,
-        Date: 1,
-        Time: 1,
-        Trainer: 1,
-        // isExpired: {
-        //   // new field
-        //   $cond: {
-        //     if: { $gt: ["$EventDeleteDate", new Date()] },
-        //     then: false,
-        //     else: true,
-        //   },
-        // },
-      },
-    },
-  ]);
+async function getCoachList(req, res) {
+  console.log("In get Coach List");
+  let NewList = await CoachesList.find({ Status: "Active" });
   console.log("first", NewList.length);
-
   return res.status(200).json({ List: NewList });
 }
 router.get("/getcoachlist", auth, async (req, res) => {
   console.log("In request Get Coach List ");
   try {
     // updateStatus();
-    getCoursList(req, res);
+    getCoachList(req, res);
     // let NewList = await CoachesList.find({ Status: "Active" });
     // console.log("first", NewList.length);
     // return res.status(200).json({ List: NewList });
@@ -91,36 +28,36 @@ router.get("/getcoachlist", auth, async (req, res) => {
   }
 });
 
-router.get("/getindividualcourse", async (req, res) => {
-  console.log("In request Get Indiv Course Data ", req.query);
-  // res.setHeader("Access-Control-Allow-Origin", "*");
+// router.get("/getindividualcourse", async (req, res) => {
+//   console.log("In request Get Indiv Course Data ", req.query);
+//   // res.setHeader("Access-Control-Allow-Origin", "*");
 
-  try {
-    let CourseData = await CoursesList.findOne(
-      { CourseID: req.query.CourseID },
-      {
-        CourseID: 1,
-        CourseName: 1,
-        EndDate: 1,
-        EventDeleteDate: 1,
-        Place: 1,
-        ActualCost: 1,
-        DiscountedCost: 1,
-        StartDate: 1,
-        Time: 1,
-        Trainer: 1,
-        _id: 0,
-      }
-    );
+//   try {
+//     let CourseData = await CoursesList.findOne(
+//       { CourseID: req.query.CourseID },
+//       {
+//         CourseID: 1,
+//         CourseName: 1,
+//         EndDate: 1,
+//         EventDeleteDate: 1,
+//         Place: 1,
+//         ActualCost: 1,
+//         DiscountedCost: 1,
+//         StartDate: 1,
+//         Time: 1,
+//         Trainer: 1,
+//         _id: 0,
+//       }
+//     );
 
-    console.log("first", CourseData);
+//     console.log("first", CourseData);
 
-    return res.status(200).json({ CourseData });
-  } catch (err) {
-    // logger.error(`Catch Block - User List Request Block ${err}`, { by: req.user.gid, for: [0], info: {} })
-    return res.status(500).json({ error: `Server Error: ${err}` });
-  }
-});
+//     return res.status(200).json({ CourseData });
+//   } catch (err) {
+//     // logger.error(`Catch Block - User List Request Block ${err}`, { by: req.user.gid, for: [0], info: {} })
+//     return res.status(500).json({ error: `Server Error: ${err}` });
+//   }
+// });
 
 router.post("/addnewcoach", auth, async (req, res) => {
   console.log("In add new Course router post request");
@@ -161,72 +98,71 @@ router.post("/addnewcoach", auth, async (req, res) => {
   }
 });
 
-router.put("/updatecourse/:CourseID", auth, async (req, res) => {
-  console.log("In request Update Indiv Course Data ", req.params, req.body);
+router.put("/updatecoach/:CoachID", auth, async (req, res) => {
   try {
-    const CourseID = req.params.CourseID;
-    const newData = req.body.data;
-    let oldData = await CoursesList.findOne({ CourseID });
-    let updatedThings = [];
-    let cnt = 0;
-
-    for (const property in newData) {
-      console.log("DDD", property);
-
-      if (`${newData[property]}` !== `${oldData[property]}`) {
-        if (property.includes("Date")) {
-          if (`${oldData[property]}` !== `${new Date(newData[property])}`) {
-            updatedThings.push({
-              keyname: property,
-              oldValue: oldData[property],
-              newValue: newData[property],
-            });
-            cnt++;
-          }
-        } else {
-          updatedThings.push({
-            keyname: property,
-            oldValue: oldData[property],
-            newValue: newData[property],
-          });
-          cnt++;
-        }
-        // updatedString += `${property} : ${oldData[property]} --> ${newData[property]} \n`;
+    uploadController.multipleUpload(req, res, function (err) {
+      if (err) {
+        console.log("Error Photo Submission", err);
+        return res.end("Error uploading file.");
       }
-    }
-    console.log("first", cnt, updatedThings);
+      console.log("In request Update Indiv User Data ", req.params, req.body);
+      async function asyncPutCall() {
+        const CoachID = req.params.CoachID;
+        const newData = req.body;
+        let oldData = await CoachesList.findOne({ CoachID });
+        console.log("DDDEEE", oldData, newData);
+        let updatedThings = [];
+        let cnt = 0;
 
-    if (cnt > 0) {
-      console.log("To update");
-      await CoursesList.updateOne(
-        { CourseID: CourseID },
-        {
-          $set: newData,
-          $push: {
-            Updation: [
-              {
-                ByID: req.user.gid,
-                ByName: req.user.name,
-                OnDate: new Date(),
-                Updates: updatedThings,
-              },
-            ],
-          },
+        for (const property in newData) {
+          if (property !== "photoURL")
+            if (`${newData[property]}` !== `${oldData[property]}`) {
+              console.log("DDD", property);
+              updatedThings.push({
+                keyname: property,
+                oldValue: oldData[property],
+                newValue: newData[property],
+              });
+              cnt++;
+            }
+          // updatedString += `${property} : ${oldData[property]} --> ${newData[property]} \n`;
         }
-      )
-        .then(async () => {
-          let enterData = await CoursesList.findOne({ CourseID });
-          console.log("The renter data", enterData);
-          getCoursList(req, res);
-        })
-        .catch((err) => {
-          console.log("errr", err);
+        console.log("first", cnt, updatedThings);
 
-          return res.status(500).json({ error: "Server Error" });
-        });
-    } else {
-      return res.json("Nothing to update");
-    }
+        if (cnt > 0) {
+          console.log("To update");
+          await CoachesList.updateOne(
+            { CoachID: CoachID },
+            {
+              $set: newData,
+              $push: {
+                Updation: [
+                  {
+                    ByID: req.user.gid,
+                    ByName: req.user.name,
+                    OnDate: new Date(),
+                    Updates: updatedThings,
+                  },
+                ],
+              },
+            }
+          )
+            .then(async () => {
+              let enterData = await CoachesList.findOne({ CoachID });
+              console.log("The renter data", enterData);
+              getCoachList(req,res);
+            })
+            .catch((err) => {
+              console.log("errr", err);
+
+              return res.status(500).json({ error: "Server Error" });
+            });
+        } else {
+          return res.json("Nothing to update");
+        }
+      }
+      asyncPutCall();
+    });
   } catch (err) {
     console.log("errr", err);
     // logger.error(`Catch Block - User List Request Block ${err}`, { by: req.user.gid, for: [0], info: {} })
@@ -234,12 +170,12 @@ router.put("/updatecourse/:CourseID", auth, async (req, res) => {
   }
 });
 
-router.delete("/DeleteCourse/:CourseID", auth, async (req, res) => {
+router.delete("/deletecoach/:CoachID", auth, async (req, res) => {
   console.log("In Delete Course", req.params, req.query);
   try {
-    const deleteCourse = req.params.CourseID;
-    await CoursesList.updateOne(
-      { CourseID: deleteCourse },
+    const deleteCourse = req.params.CoachID;
+    await CoachesList.updateOne(
+      { CoachID: deleteCourse },
       {
         $set: {
           Status: "Deleted",
@@ -251,7 +187,7 @@ router.delete("/DeleteCourse/:CourseID", auth, async (req, res) => {
       }
     );
 
-    getCoursList(req, res);
+    getCoachList(req, res);
   } catch (err) {
     console.log("errr", err);
     return res.status(500).json({ error: `Server Error: ${err}` });
